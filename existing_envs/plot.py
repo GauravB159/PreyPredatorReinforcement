e41935f5e2c12b37b51097f29176c6ebdca5a2a2
Krithika <krithikabq@gmail.com>
2024-04-05 19:44:06 -0700

    add model and logs

--- existing_envs/plot.py
+++ existing_envs/plot.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the data from the CSV file
-data = pd.read_csv('existing_envs/log_training_1712282292.csv')
+data = pd.read_csv('existing_envs/log_test_20240404-210735.csv')
 
 print(data.columns)
 # Assume the CSV file has two columns 'x' and 'y'
-plt.plot(data['Episode'], data[' Episodic Return'])
+plt.plot(data['Episode'], data['Test_Return'])
 
 # Show the plot
 plt.show()
